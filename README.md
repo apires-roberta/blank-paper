a8447cffa2bcbf48a46f09a72ed17fd0e6a511f3
apires-roberta <roberta.pires@bandtec.com.br>
2021-05-14 01:38:54 -0300

    Adicionando pasta bd

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projeto-site-instituicional-PI
-Construção de um site institucional para uma empresa fictícia. 
---
+++ banco-de-dados/script-blank-paper.sql
@@ -0,0 +1,14 @@
+create database blank_paper;
+
+use blank_paper;
+
+CREATE TABLE usuario(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    login VARCHAR(70),
+    senha VARCHAR(45),
+    textarea varchar(140)
+);
+
+select * from usuario;
+
