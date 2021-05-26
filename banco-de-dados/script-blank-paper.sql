ae7124e2408b8ef72d61639c979362d025a9f5ff
apires-roberta <roberta.pires@bandtec.com.br>
2021-05-25 22:38:52 -0300

    Atualizando página about me

--- banco-de-dados/script-blank-paper.sql
+++ banco-de-dados/script-blank-paper.sql
@@ -1,5 +1,4 @@
 create database blank_paper;
-
 use blank_paper;
 
 CREATE TABLE usuario(
@@ -9,31 +8,41 @@ CREATE TABLE usuario(
     senha VARCHAR(45)
 );
 
-
-alter table usuario add constraint unique (login);
-
 CREATE TABLE comissoes(
-    idComissoes INT PRIMARY KEY AUTO_INCREMENT,
+    idComissoes INT AUTO_INCREMENT,
     valueCheck VARCHAR(6),
+    foreign key (valueCheck) references tipoComissoes(nomeTipo),
     textarea_conteudo VARCHAR(140),
     fkUsuario VARCHAR(70),
-    foreign key (fkUsuario) references usuario(login)
+    foreign key (fkUsuario) references usuario(login),
+    primary key (idComissoes, valueCheck, fkUsuario)
 ) auto_increment = 1000;
 
+create table tipoComissoes(
+id_tipoComissoes int primary key auto_increment,
+nomeTipo VARCHAR(6) unique key,
+valor int
+) auto_increment = 2000;
 
-select * from comissoes;
+insert into tipoComissoes value (null, 'basic', 5);
+insert into tipoComissoes value (null, 'medium', 15);
+insert into tipoComissoes value (null, 'plus', 25);
+
+update usuario set login = 'Pedrin2002@gmail.com' where id = 24;
 select * from usuario;
+select * from comissoes;
+select * from tipoComissoes;
 
-select * from comissoes join usuario where fkUsuario = login;
-select usuario.login, comissoes.textarea_conteudo, comissoes.valueCheck from usuario 
-join comissoes where usuario.login = comissoes.fkUsuario and usuario.login = 'jorge@gmail.com';
+select id, nome, login, idComissoes as 'Número do Pedido', valueCheck as 'Nome do Pacote', textarea_conteudo as 'Descrição', valor from usuario 
+join comissoes on usuario.login = comissoes.fkUsuario
+join tipoComissoes on nomeTipo = valueCheck;
 
-select max(valueCheck) as 'Pacote com menos requisição', min(valueCheck) as 'Pacote com mais requisição' from comissoes;
+select min(valueCheck) as 'Pacote com menos requisição', max(valueCheck) as 'Pacote com mais requisição' from comissoes;
+
 select count(valueCheck) as 'Quantidade de pacotes' from comissoes where valueCheck = 'basic';
 select count(valueCheck) as 'Quantidade de pacotes' from comissoes where valueCheck = 'medium';
 select count(valueCheck) as 'Quantidade de pacotes' from comissoes where valueCheck = 'plus';
- 
-select * from comissoes inner join usuario on fkUsuario = login and valueCheck = 'basic';
-select * from comissoes inner join usuario on fkUsuario = login and valueCheck = 'medium';
-select * from comissoes inner join usuario on fkUsuario = login and valueCheck = 'plus';
+
+select count(valueCheck) from comissoes;
+
 
